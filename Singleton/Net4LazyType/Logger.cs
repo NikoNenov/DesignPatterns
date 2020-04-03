d59da66f226f61df069447daefb6945b589c1ae8
Niko Nenov <nikolay@nenov.de>
2020-04-03 18:00:24 +0200

    Add singleton design pattern

---
+++ Singleton/Net4LazyType/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Nenov.DesignPatterns.Singleton.Net4LazyType
+{
+  /// <summary>
+  ///  .NET 4 (or higher), you can use the System.Lazy<T> type to make the laziness
+  /// </summary>
+  public sealed class Logger
+  {
+    private static readonly Lazy<Logger> lazy = new Lazy<Logger>(() => new Logger());
+
+    public static Logger Instance => lazy.Value;
+
+    private Logger()
+    {
+    }
+  }
+}
---
+++ Singleton/NotThreadSafe/Logger.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Nenov.DesignPatterns.Singleton.NotThreadSafe
+{
+  /// <summary>
+  /// Not thread-safe.
+  /// Two different threads could both have evaluated the test if (instance==null) and found it to be true,
+  /// then both create instances, which violates the singleton pattern.
+  /// </summary>
+  public class Logger
+  {
+    private static Logger _instance;
+
+    protected Logger() { }
+
+    /// <summary>
+    /// Get the logger singleton instance
+    ///
+    /// Uses lazy initialization and is not thread safe.
+    /// </summary>
+    /// <returns>logger instance</returns>
+    public static Logger Instance()
+    {
+      return _instance ?? (_instance = new Logger());
+    }
+  }
+}
